91ebd5bde059a33beb5cda5a020ae4e19d8fe456
nyjgary86 <garyng@wireless-245-153.uchicago.edu>
2014-11-06 22:31:11 -0600

    Details Page
    First tiny part done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,11 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    the_id = params[:id]
+    the_photo = Photo.find_by(id: the_id)
+    @the_caption = the_photo.caption
+    @the_source = the_photo.source
+  end
 
 end
